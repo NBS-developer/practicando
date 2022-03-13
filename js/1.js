fa754bf2491fe3582f8407f009bd6a9244bfe3b7
Javier D'Alessandro <pekes3her@gmailcom>
2022-03-13 11:10:56 -0300

    muchos cambios

--- js/1.js
+++ js/1.js
@@ -15,4 +15,20 @@ $(".e-button").on("click", function() {
     }
 });
 
-/*slider*/
+/*msj*/
+const frm = document.querySelector("#frm");
+
+frm.addEventListener("submit", event => {
+    event.preventDefault();
+    const frm = event.target;
+    const formData = {
+        name: frm.name.value,
+        phone: frm.phone.value,
+        /*  email: frm.email.value,
+          date: frm.date.value*/
+    };
+
+    const URL = `https://api.whatsapp.com/send?phone=5493412295930&text=Que tal NBS.STYLES quierolaremera de%20en color,${formData.name}%20y %20en%20talle,${formData.phone}%20gg%20&source=&data=`;
+
+    window.open(URL, "_blank");
+});
